16f02212007a0530b1e67e0073d3ecb4cb6fd6d5
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-10-18 19:06:45 -0300

    Estrutura e designe

--- src/Dashboard/styles.ts
+++ src/Dashboard/styles.ts
@@ -1,15 +1,42 @@
 import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
     flex: 1;
-    justify-content: center;
-    align-items: center;
     background-color: ${({theme}) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
-  color: ${({theme}) => theme.colors.title};
-  font-family: ${({theme}) => theme.fonts.bold};
-  font-size: 24px;
-  font-weight: bold;
+export const Header = styled.View`
+  align-items: center;
+  background-color: ${({theme}) => theme.colors.primary};
+  flex-direction: row;
+  height: ${RFPercentage(42)}px;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const UserWrapper = styled.View`
+  padding: 0 24px;
+  width: 100%;
+`;
+
+export const UserInfo = styled.View`
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const Photo = styled.Image`
+
+`;
+
+export const User = styled.View`
+
+`;
+
+export const UserGreeting = styled.Text`
+
+`;
+
+export const UserName = styled.Text`
+
 `;
