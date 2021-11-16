9c2522e701b83bb65ed61903f4e2e29d967e0c94
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-11-15 21:00:12 -0300

    Finished

--- App.tsx
+++ App.tsx
@@ -1,31 +1,44 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
 
+import React from 'react';
+import { StatusBar } from 'react-native'; 
+import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
+
+import { Routes } from './src/routes';
+
 import {
+  useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
-  Poppins_700Bold,
-  useFonts,
+  Poppins_700Bold
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
 
-import { Dashboard } from './src/screens/Dashboard';
-import AppLoading from 'expo-app-loading';
+import { SignIn } from './src/screens/SignIn';
+
+import { AuthProvider } from './src/hooks/auth';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
-    Poppins_700Bold,
+    Poppins_700Bold
   });
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
+  if(!fontsLoaded){
+    return <AppLoading />
   }
+
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <StatusBar barStyle="light-content" />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
-  );
+  )
 }
--- README.md
+++
@@ -1,3 +0,0 @@
-# Gofinances
-## React Native - Finances APP
-App desenvolvido durante o chapter 2 do curso de React Native da Rocketseat
